c6017ddaf79607db170aa113d72c3a93b95d0980
Deniz Misirlioglu <thezedplays@gmail.com>
2023-11-21 21:49:16 -0500

    Commit

--- hamming.py
+++ hamming.py
@@ -2,24 +2,45 @@ import numpy as np
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
-#Generation
-bit_matrix = np.random.randint(0,2,10000)
+
+bit_matrix = np.random.randint(0, 2, 10000)
 
 G = np.array([[1, 0, 0, 0, 1, 1, 0],
-              [0, 1, 0, 0, 1, 0, 1], 
-              [0, 0, 1, 0, 0, 1, 1], 
+              [0, 1, 0, 0, 1, 0, 1],
+              [0, 0, 1, 0, 0, 1, 1],
               [0, 0, 0, 1, 1, 1, 1]])
 
+D = np.array([[1,1,1],
+              [1,1,0],
+              [1,0,1],
+              [0,1,1],
+              [1,0,0],
+              [0,1,0],
+              [0,0,1]])
 
 def encodeFourBits(four_bits):
+    four_bits = four_bits.reshape(1, -1)
     return np.dot(four_bits, G) % 2
-   
-encoded_bits= []
+
+def decodeSevenBits(seven_bits):
+    seven_bits = seven_bits.reshape(1, -1)
+    return np.dot(seven_bits, D) % 2
+
+encoded_bits = []
+decoded_bits = []
 
 for i in range(0, len(bit_matrix), 4):
-    four_bits = bit_matrix[i:i+4];
-    print(four_bits)
-    encoded_bits += encodeFourBits(four_bits)
-    print(encoded_bits)
-    
+    four_bits = bit_matrix[i:i + 4]
+    encoded_bits.extend(encodeFourBits(four_bits))
 
+
+
+for seven_bits in encoded_bits:
+    decoded_bits.extend(decodeSevenBits(seven_bits))
+
+
+encoded_bits = np.vstack(encoded_bits)
+decoded_bits = np.vstack(decoded_bits)
+
+print("Encoded bits:")
+print(decoded_bits)

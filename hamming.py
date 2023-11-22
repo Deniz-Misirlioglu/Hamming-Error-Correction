c70046579afaf2db7abebc0f7bfc37db0bb8946d
Deniz Misirlioglu <thezedplays@gmail.com>
2023-11-21 22:25:03 -0500

    Commit

--- hamming.py
+++ hamming.py
@@ -24,20 +24,42 @@ def encodeFourBits(four_bits):
 
 def decodeSevenBits(seven_bits):
     seven_bits = seven_bits.reshape(1, -1)
-    print("THIS IS DECODED " ,np.dot(seven_bits, D) % 2)
     return np.dot(seven_bits, D) % 2
 
 encoded_bits = []
 decoded_bits = []
 
+#Encoding the bits and setting encoded_bits to a 7,2500 matrix of bits
 for i in range(0, len(bit_matrix), 4):
     four_bits = bit_matrix[i:i + 4]
     encoded_bits.extend(encodeFourBits(four_bits))
 
 
+def errorCalculation(p, matrix):
+    errorCount = 0;
+    for seven_bits in matrix:
+        for i in range(len(seven_bits)):
+            random_number_np = round(np.random.rand(), 2)
+            if random_number_np <= p:
+                errorCount = errorCount + 1
+                if seven_bits[i] == 1:
+                    seven_bits[i] = 0
+                else:
+                    seven_bits[i] = 1
+    return errorCount
+
+p = [0.01,0.05,0.1,0.2]
+
+for i in p:
+    tempMatrix = encoded_bits;
+    errorMatrix = errorCalculation(i,tempMatrix)
+    print(errorMatrix)
+
 
 for seven_bits in encoded_bits:
     decoded_bits.extend(decodeSevenBits(seven_bits))
+
+
 
 
 encoded_bits = np.vstack(encoded_bits)

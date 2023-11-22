eec2d567be683a6f7b06703cc03f33aaa1e24471
Deniz Misirlioglu <thezedplays@gmail.com>
2023-11-21 21:36:49 -0500

    Commit

--- hamming.py
+++ hamming.py
@@ -1,9 +1,25 @@
 import numpy as np
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 #Generation
-bit_matrix = np.random.randint(2, size=(1, 10000))
-print(bit_matrix)
+bit_matrix = np.random.randint(0,2,10000)
 
-#Encode
-for bits in bit_matrix:
-    print(bits)
+G = np.array([[1, 0, 0, 0, 1, 1, 0],
+              [0, 1, 0, 0, 1, 0, 1], 
+              [0, 0, 1, 0, 0, 1, 1], 
+              [0, 0, 0, 1, 1, 1, 1]])
+
+
+def encodeFourBits(four_bits):
+    return np.dot(four_bits, G) % 2
+   
+encoded_bits= []
+
+for i in range(0, len(bit_matrix), 4):
+    four_bits = bit_matrix[i:i+4];
+    print(four_bits)
+    encoded_bits += encodeFourBits(four_bits)
+    print(encoded_bits)
+    
+
